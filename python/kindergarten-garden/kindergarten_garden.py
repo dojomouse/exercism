d583ee0bfd41d31fe284acde8152de45982e1eb3
Nick Butcher <nick.butcher@gmail.com>
2020-07-18 21:41:43 +1200

    Tidyup

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,17 +1,13 @@
 class Garden:
-    def __init__(self, diagram, students=["Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Illeana", "Joseph", "Kincaid", "Larry"]):
+    STUDENTS = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred', 'Ginny', 'Harriet', 'Illeana', 'Joseph', 'Kincaid', 'Larry']
+    PLANT_CODES = {'V': 'Violets', 'R': 'Radishes', 'G':'Grass', 'C':'Clover'}
+    def __init__(self, diagram, students=STUDENTS):
         students.sort()
-        self.ChildsPlants = {child: [] for child in students}
+        self.StudentPlants = {student: [] for student in students}
         for row in diagram.splitlines():
             for idx, plantCode in enumerate(row):
-                self.ChildsPlants[students[idx//2]].append(plantCode)
+                self.StudentPlants[students[idx//2]].append(plantCode)
 
-    def code_to_plant(self, char):
-        PLANT_CODES = {"V": "Violets", "R": "Radishes", "G":"Grass", "C":"Clover"}
-        return PLANT_CODES[char]
-
-    def plants(self, child):
-        print(self.ChildsPlants)
-        print(self.ChildsPlants[child])
-        return [self.code_to_plant(code) for code in self.ChildsPlants[child]]
+    def plants(self, student):
+        return [self.PLANT_CODES[code] for code in self.StudentPlants[student]]
             
